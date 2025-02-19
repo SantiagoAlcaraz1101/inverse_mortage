38e4f508bb471bbf59e99ee9dfdef881dac0a9cf
Juan <112285842+Josecopro@users.noreply.github.com>
2025-02-19 14:13:04 +0000

    New: test.py added

---
+++ tests/test.py
@@ -0,0 +1,17 @@
+import unittest
+
+class InverseMortage(unittest.TestCase):
+
+    def 
+
+    
+    
+    def NormalTestCase1(self):
+        
+        Age: int
+        stratum: int 
+        CommercialValue: float
+        Antiqueness: int
+        Sex: bool
+        IsLegal: bool
+
