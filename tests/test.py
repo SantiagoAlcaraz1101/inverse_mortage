22422e6ba910d3226d28d5d1c9c055b89aca283b
Juan <112285842+Josecopro@users.noreply.github.com>
2025-02-21 13:43:05 +0000

    add:  test case 1 logic and refactor conditions to apply

--- tests/test.py
+++ tests/test.py
@@ -1,37 +1,30 @@
 import unittest
 
+from src.check_process import is_old_enough, required_stratum, is_value_enough, is_property_valid, hope_of_life
+
+normal_testcase_1 = (65, 4, 655e5, 25, True, True)
+
+
 class inverse_mortage(unittest.TestCase):
 
-    def is_old_enough(self, age: int) -> bool:
-        return age >=65
-    
-    def required_stratum(self, stratum: int) -> bool:
-        return stratum >= 4
-
-    def is_value_enough(self, commercial_value: float) -> bool:
-        return commercial_value >= 655e5
-    
-    def hope_of_life(self, age: int, is_women: bool) -> int:
-        if is_women:
-            life_expentancy: int = 78
-            if life_expentancy < age:
-                return 0 
-
-            return life_expentancy - age
-        else:
-            life_expentancy: int = 71
-            if life_expentancy < age:
-                return 0 
-            return life_expentancy - age
-    
-    def is_property_valid(self, antiqueness:int ):
-        return antiqueness <= 25
     
 
-    
-    
     def normal_testcase_1(self, age: int, stratum: int, commercial_value: float, antiqueness: int, is_women: bool, is_legal: bool):
-        pass 
+
+        if is_old_enough(age) and required_stratum(stratum) and is_value_enough(commercial_value) and is_property_valid(antiqueness) and hope_of_life(age, is_women) and is_legal:
+            return True
+        return False
+    
+    def normal_testcase_value1():
+        pass
+
+    
+    def test_normal_testcase_1(self):
+        age, stratum, commercial_value, antiqueness, is_women, is_legal = normal_testcase_1
+        result = self.normal_testcase_1(age, stratum, commercial_value, antiqueness, is_women, is_legal)
+        self.assertTrue(result)
+    
+         
         
 
 
