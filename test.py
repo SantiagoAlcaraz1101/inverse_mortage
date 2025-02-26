28e1f38f199fc8ac248e982c439eebfb0004e57c
Juan <112285842+Josecopro@users.noreply.github.com>
2025-02-26 14:18:39 +0000

    new: added better test

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import unittest
 from inverse_mortage import Person, Property
-from check_process import is_old_enough, required_stratum, is_value_enough, is_property_valid, hope_of_life
 
 
 normal_testcase_Property_1 = Property(4, 655e5, 25, True)
@@ -20,65 +19,36 @@ extraordinary_testcase_Person_1 = Person("Pepito Perez Vaquero", 85, True, False
 
 
 class inverse_mortage(unittest.TestCase):
+    
+    def test_normal_1(self):
+        self.assertTrue(normal_testcase_Person_1.is_old_enough())
+        self.assertTrue(normal_testcase_Property_1.required_stratum())
+        self.assertTrue(normal_testcase_Property_1.is_value_enough())
+        self.assertTrue(normal_testcase_Property_1.is_property_valid())
+        self.assertEqual(normal_testcase_Person_1.hope_of_life(), 13)
+
+    def test_normal_2(self):
+        self.assertTrue(normal_testcase_Person_2.is_old_enough())
+        self.assertTrue(normal_testcase_Property_2.required_stratum())
+        self.assertTrue(normal_testcase_Property_2.is_value_enough())
+        self.assertTrue(normal_testcase_Property_2.is_property_valid())
+        self.assertEqual(normal_testcase_Person_2.hope_of_life(), 12)
+
+    def test_normal_3(self):
+        self.assertTrue(normal_testcase_Person_3.is_old_enough())
+        self.assertTrue(normal_testcase_Property_3.required_stratum())
+        self.assertTrue(normal_testcase_Property_3.is_value_enough())
+        self.assertTrue(normal_testcase_Property_3.is_property_valid())
+        self.assertEqual(normal_testcase_Person_3.hope_of_life(), 7)
+
+    def test_extraordinary_1(self):
+        self.assertTrue(extraordinary_testcase_Person_1.is_old_enough())
+        self.assertTrue(extraordinary_testcase_Property_1.required_stratum())
+        self.assertTrue(extraordinary_testcase_Property_1.is_value_enough())
+        self.assertTrue(extraordinary_testcase_Property_1.is_property_valid())
+        self.assertEqual(extraordinary_testcase_Person_1.hope_of_life(), 0)
 
     
 
-    def normal_testcase_1(self, age: int, stratum: int, commercial_value: float, antiqueness: int, is_women: bool, is_legal: bool):
-
-        if is_old_enough(age) and required_stratum(stratum) and is_value_enough(commercial_value) and is_property_valid(antiqueness) and hope_of_life(age, is_women) and is_legal:
-            return True
-        return False
-    
-    def normal_testcase_value1():
-        pass
-
-    #Verifica si puede acceder a la hipoteca inversa
-    def test_normal_testcase_1(self):
-        age, stratum, commercial_value, antiqueness, is_women, is_legal = normal_testcase_1
-        result = self.normal_testcase_1(age, stratum, commercial_value, antiqueness, is_women, is_legal)
-        self.assertTrue(result)
-
-    #Caso normal 2
-    def normal_testcase_2(self, age: int, stratum: int, commercial_value: float, antiqueness: int, is_women: bool, is_legal: bool):
-
-        if is_old_enough(age) and required_stratum(stratum) and is_value_enough(commercial_value) and is_property_valid(antiqueness) and hope_of_life(age, is_women) and is_legal:
-            return True
-        return False
-    
-    def normal_testcase_value2():
-        pass
-
-    #Verifica si puede acceder a la hipoteca inversa
-    def test_normal_testcase_2(self):
-        age, stratum, commercial_value, antiqueness, is_women, is_legal = normal_testcase_2
-        result = self.normal_testcase_2(age, stratum, commercial_value, antiqueness, is_women, is_legal)
-        self.assertTrue(result)
-
-    #Caso normal 3
-    def normal_testcase_3(self, age: int, stratum: int, commercial_value: float, antiqueness: int, is_women: bool, is_legal: bool):
-
-        if is_old_enough(age) and required_stratum(stratum) and is_value_enough(commercial_value) and is_property_valid(antiqueness) and hope_of_life(age, is_women) and is_legal:
-            return True
-        return False
-    
-    def normal_testcase_value3():
-        pass
-
-    #Verifica si puede acceder a la hipoteca inversa
-    def test_normal_testcase_3(self):
-        age, stratum, commercial_value, antiqueness, is_women, is_legal = normal_testcase_3
-        result = self.normal_testcase_3(age, stratum, commercial_value, antiqueness, is_women, is_legal)
-        self.assertTrue(result)
-
-    
-    def extraordinary_testcase_1(self, age: int):
-
-        if is_old_enough(age): #and required_stratum(stratum) and is_value_enough(commercial_value) and is_property_valid(antiqueness) and hope_of_life(age, is_women)== 0 and is_legal:
-            return True
-        return False
-
-    def test_extraordinary_testcase_1(self):
-        age= 85
-        result = self.extraordinary_testcase_1(age)
-        self.assertTrue(result)
-
+if __name__ == "__main__":
+    unittest.main()
