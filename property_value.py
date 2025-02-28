750491a3bef025af085d01d044244d371e859818
Juan <112285842+Josecopro@users.noreply.github.com>
2025-02-28 13:47:07 +0000

    New: module added to calculate the monthly payments

--- property_value.py
+++ property_value.py
@@ -1,25 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+
+def property_value(T: int, initial_value: float):
 # Parámetros
-V0 = 100  # valor inicial
-mu = 0.05  # tasa de retorno
-sigma = 0.2  # volatilidad
-T = 1  # tiempo total en años
-dt = 0.01  # tamaño del paso de tiempo
-n_steps = int(T / dt)  # número de pasos
-t = np.linspace(0, T, n_steps)  # vector de tiempos
 
-# Generación de un proceso de Wiener (ruido blanco)
-W = np.random.normal(0, np.sqrt(dt), size=n_steps)  # Incrementos de Wiener
-W = np.cumsum(W)  # Sumando los incrementos para obtener el proceso de Wiener
+    mu = 0.034  # tasa de retorno
+    sigma = 0.1  # volatilidad
+    dt = 0.01  # tamaño del paso de tiempo
+    n_steps = int(T / dt)  # número de pasos
+    t = np.linspace(0, T, n_steps)  # vector de tiempos
 
-# Cálculo de Vt según la fórmula
-V = V0 * np.exp((mu - 0.5 * sigma**2) * t + sigma * W)
+    # Generación de un proceso de Wiener (ruido blanco)
+    W = np.random.normal(0, np.sqrt(dt), size=n_steps)  # Incrementos de Wiener
+    W = np.cumsum(W)  # Sumando los incrementos para obtener el proceso de Wiener
+
+    # Cálculo de Vt según la fórmula
+    V = initial_value * np.exp((mu - 0.5 * sigma**2) * t + sigma * W)
+
+    return(V[-1]/T/12)
 
 # Graficando el resultado
-plt.plot(t, V)
-plt.title("Simulación de Geometric Brownian Motion")
-plt.xlabel("Tiempo (t)")
-plt.ylabel("Valor (V_t)")
-plt.show()
+#plt.plot(t, V)
+#plt.title("Simulación de Geometric Brownian Motion")
+#plt.xlabel("Tiempo (t)")
+#plt.ylabel("Valor (V_t)")
+#plt.show()
